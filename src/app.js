ea0e489706d9314cfe767bf4024690670f5c579c
Kartik <9415kks@gmail.com>
2022-02-21 01:00:14 +0530

    Adding the Delete functionality

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.post("/mens", async (req, res) => {
 // we will handle get req
 app.get("/mens", async (req, res) => {
   try {
-    const getMens = await MensRanking.find({});
+    const getMens = await MensRanking.find({}).sort({ ranking: 1 });
     res.send(getMens);
   } catch (e) {
     console.log(e);
@@ -48,11 +48,25 @@ app.get("/mens/:id", async (req, res) => {
 app.patch("/mens/:id", async (req, res) => {
   try {
     const _id = req.params.id;
-    const getMen = await MensRanking.findByIdAndUpdate(_id, req.body);
+    const getMen = await MensRanking.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
     res.send(getMen);
   } catch (e) {
     console.log(e);
-    res.status(400).send(e);
+    res.status(500).send(e);
+  }
+});
+
+// we will handle delete req of indiv
+app.delete("/mens/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const getMen = await MensRanking.findByIdAndDelete(_id);
+    res.send(getMen);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
   }
 });
 
