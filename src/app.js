e41bd49831887e3928dfca9421273aa96ffde69b
Kartik <9415kks@gmail.com>
2022-02-21 01:08:04 +0530

    Completed api

--- src/app.js
+++ src/app.js
@@ -2,77 +2,13 @@ const express = require("express");
 require("../src/db/conn");
 
 const MensRanking = require("./models/mens");
+const router = require("../src/routers/men");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-// we will handle post req
-app.post("/mens", async (req, res) => {
-  try {
-    const addingMensRecords = new MensRanking(req.body);
-    console.log(req.body);
-    const insertMens = await addingMensRecords.save();
-    res.status(201).send(insertMens);
-  } catch (e) {
-    console.log(e);
-    res.status(400).send(e);
-  }
-});
-
-// we will handle get req
-app.get("/mens", async (req, res) => {
-  try {
-    const getMens = await MensRanking.find({}).sort({ ranking: 1 });
-    res.send(getMens);
-  } catch (e) {
-    console.log(e);
-    res.status(400).send(e);
-  }
-});
-
-// we will handle get req of indiv
-app.get("/mens/:id", async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const getMen = await MensRanking.findById({ _id });
-    res.send(getMen);
-  } catch (e) {
-    console.log(e);
-    res.status(400).send(e);
-  }
-});
-
-// we will handle patch req of indiv
-app.patch("/mens/:id", async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const getMen = await MensRanking.findByIdAndUpdate(_id, req.body, {
-      new: true,
-    });
-    res.send(getMen);
-  } catch (e) {
-    console.log(e);
-    res.status(500).send(e);
-  }
-});
-
-// we will handle delete req of indiv
-app.delete("/mens/:id", async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const getMen = await MensRanking.findByIdAndDelete(_id);
-    res.send(getMen);
-  } catch (e) {
-    console.log(e);
-    res.status(500).send(e);
-  }
-});
-
-app.get("/", async (req, res) => {
-  res.send("Hello World");
-});
+app.use(router);
 
 app.listen(port, () => {
   console.log(`connection is live at port no. ${port}`);
