bbce0f6c1ee10f794d6fc5078adc20c1a0752b9d
Kartik <9415kks@gmail.com>
2022-02-20 20:05:56 +0530

    Started making the rest Api

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require('../src/db/conn')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`connection is live at port no. ${port}`);
+});
